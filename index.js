1e99523538b9768a276c3c1e59b7c87ca7794083
Pedro Ventura <pedrovnt@gmail.com>
2023-05-22 18:58:08 -0700

    add and minus

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){ 
+    return a+b;
+}
+function subtract(a, b){
+    return a-b;
+    }
+function multiply(a, b){
+    return a*b
+}
+function divide(a, b){
+    return a/b
+}
+function increment(a) {
+    a++
+    return a
+}
+function decrement(a) {
+    --a
+    return a
+}
+
+function makeInt(string) {
+  return  parseInt(string, 10) 
+}
+function preserveDecimal(string){
+    return parseFloat(string,2.222)
+     }
